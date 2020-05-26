e4c4c09a5a67b20315e19152267d8ad1b0e32d16
Usuario <Usuario@DESKTOP-UKKV8R5.lan>
2020-05-25 20:51:20 -0500

    tercer commit

--- SpringDemoApp/src/main/java/idat/edu/pe/demo/SpringDemoAppApplication.java
+++ SpringDemoApp/src/main/java/idat/edu/pe/demo/SpringDemoAppApplication.java
@@ -8,7 +8,7 @@ public class SpringDemoAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoAppApplication.class, args);
-		System.out.println("Mi proyecto Spring con Spring Boot 2020 2");       
+		System.out.println("Mi proyecto Spring con Spring Boot 2020 23");        
 		
 		
 	}
