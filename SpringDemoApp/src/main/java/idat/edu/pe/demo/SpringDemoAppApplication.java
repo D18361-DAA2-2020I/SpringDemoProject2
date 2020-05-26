5e6c5fc43be9abf12a0beb6bd39e9cd6b24ad64d
Usuario <Usuario@DESKTOP-UKKV8R5.lan>
2020-05-25 20:45:22 -0500

    Primer commit desde mi STS hacia mi repo en GitHub, clase del 25-05

---
+++ SpringDemoApp/src/main/java/idat/edu/pe/demo/SpringDemoAppApplication.java
@@ -0,0 +1,16 @@
+package idat.edu.pe.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoAppApplication.class, args);
+		System.out.println("Mi proyecto Spring con Spring Boot 2020");       
+		
+		
+	}
+
+}
